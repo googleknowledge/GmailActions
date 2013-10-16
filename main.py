896dc0cebe933a6a9a3145cb80140a9424920b14
devnook <devnook@gmail.com>
2013-10-16 10:39:45 +0100

    c

--- main.py
+++ main.py
@@ -29,7 +29,10 @@ import urllib2
 
 from urlparse import urlparse
 #import elementtree.ElementTree as ET
-import xml.etree.ElementTree as ET
+
+from google.appengine.api import users
+from google.appengine.api import mail
+
 
 
 jinja_environment = jinja2.Environment(
@@ -41,71 +44,20 @@ jinja_environment = jinja2.Environment(
 class MainHandler(webapp2.RequestHandler):
   def get(self):
     template = jinja_environment.get_template('index.html')
-    self.response.out.write(template.render())
+    login_url = users.create_login_url('/')
+    logout_url = users.create_logout_url('/')
+    self.response.out.write(template.render(login_url=login_url, logout_url=logout_url))
 
 
-class CseHandler(webapp2.RequestHandler):
-  def get(self):
-
-
-    #api_key = open(".freebase_api_key").read()
-    api_key = 'AIzaSyDOtKtaEkS2HLzn38knX6FDwtbP61G84ig'
-    service_url = 'https://www.googleapis.com/freebase/v1/topic'
-    #topic_id = '/m/010q36'
-    topic_id = self.request.get('mid')
-    params = {
-      'key': api_key,
-      'filter': 'suggest',
-      'filter': '/common/topic/topic_equivalent_webpage'
-    }
-    url = service_url + topic_id + '?' + urllib.urlencode(params)
-    topic = json.loads(urllib.urlopen(url).read())
-
-    #print topic
-    """
-    for property in topic['property']:
-      print property + ':'
-      for value in topic['property'][property]['values']:
-        print ' - ' + value['text']
-    """
-
-    template = jinja_environment.get_template('annotations.xml')
-
-    root = ET.Element("html")
-
-    head = ET.SubElement(root, "head")
-
-    title = ET.SubElement(head, "title")
-    title.text = "Page Title"
-
-    body = ET.SubElement(root, "body")
-    body.set("bgcolor", "#ffffff")
-
-    body.text = "Hello, World!"
-
-    # wrap it in an ElementTree instance, and save as XML
-    tree = ET.ElementTree(root)
-    print dir(ET)
-
-    xmlstr = ET.dump(tree)
-    print xmlstr
-
-
-
-    values = topic['property']['/common/topic/topic_equivalent_webpage']['values']
-    for value in values:
+  def post(self):
+    email = users.get_current_user().email()
+    if not mail.is_email_valid(email):
       pass
-      #print value['value']
-      #o = urlparse(value['value'])
-      #print '%s/' % o['netloc']
-      #print o.path.split('/')[1:-1]
-      #print '%s%s*' % (o.netloc, '/'.join(o.path.split('/')[:-1]))
-
-
-
-    self.response.headers['Content-Type'] = 'application/xml'
-
-    self.response.out.write(template.render(topic=topic, values=values))
+    else:
+      subject = "Confirm your registration"
+      content = self.request.get('email-content')
+      body = content
+      mail.send_mail(email, email, subject, body='', html=body)
 
 
 
@@ -114,6 +66,5 @@ class CseHandler(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/cse', CseHandler)
+    ('/', MainHandler)
 ], debug=True)
